b7fc7b84ac024061e3b04f32c6be4f670035654f
gaya_K <gayak.andhisbike@gmail.com>
2016-10-11 22:04:06 +0900

    Add default using spaces.

--- ScriptOnXaml/ScriptCompiler.cs
+++ ScriptOnXaml/ScriptCompiler.cs
@@ -14,7 +14,24 @@ namespace ScriptOnXaml
         {
             var domain = AppDomain.CurrentDomain;
 
-            _options = ScriptOptions.Default.WithReferences(domain.GetAssemblies());
+            _options = ScriptOptions.Default
+                .AddReferences(domain.GetAssemblies())
+                .AddImports("System")
+                .AddImports("System.Collections.Generic")
+                .AddImports("System.Linq")
+                .AddImports("System.Text")
+                .AddImports("System.Threading.Tasks")
+                .AddImports("System.Windows")
+                .AddImports("System.Windows.Controls")
+                .AddImports("System.Windows.Controls.Primitives")
+                .AddImports("System.Windows.Data")
+                .AddImports("System.Windows.Documents")
+                .AddImports("System.Windows.Input")
+                .AddImports("System.Windows.Media")
+                .AddImports("System.Windows.Media.Animation")
+                .AddImports("System.Windows.Navigation")
+                .AddImports("System.Windows.Shapes")
+                .AddImports("System.Windows.Shell");
 
             AppDomain.CurrentDomain.AssemblyLoad += (_, e) =>
             {
